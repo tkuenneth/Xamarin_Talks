da916418f99d872c89f2f11285d10690992e10f4
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2017-06-24 12:29:43 +0200

    Project TimerTaskDemo added

---
+++ TimerTaskDemo/TimerTaskDemo/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Java.Util;
+
+namespace TimerTaskDemo
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+
+        internal TextView label;
+
+        private Timer timer;
+        private TimerTask timerTask;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            label = FindViewById<TextView>(Resource.Id.label);
+            timer = new Timer();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            timerTask = new MyTask(this);
+            timer.ScheduleAtFixedRate(timerTask, 3000, 3000);
+
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            timerTask.Cancel();
+        }
+    }
+}
+
---
+++ TimerTaskDemo/TimerTaskDemo/MyTask.cs
@@ -0,0 +1,29 @@
+﻿using Android.Util;
+using Java.Util;
+
+namespace TimerTaskDemo
+{
+    public class MyTask : TimerTask
+    {
+
+        static readonly string TAG = typeof(MyTask).Name;
+
+        private int counter = 0;
+
+        private MainActivity _activity;
+        public MyTask(MainActivity activity)
+        {
+            this._activity = activity;
+        }
+
+        public override void Run()
+        {
+            _activity.RunOnUiThread(() =>
+            {
+                var s = $"{counter++}";
+                Log.Debug(TAG, s);
+                _activity.label.Text = s;
+            });
+        }
+    }
+}
