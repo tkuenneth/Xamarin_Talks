92192605526eb3fe3b85da697b7495e1ad379df9
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2017-06-24 14:00:54 +0200

    AsyncTaskDemo added

---
+++ AsyncTaskDemo/AsyncTaskDemo/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AsyncTaskDemo
+{
+    [Activity(Label = "AsyncTaskDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private Android.Views.View v;
+        private AsyncTask t;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+            v = FindViewById<Android.Views.View>(Resource.Id.view);
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+            t = new MyAsyncTask(v).Execute();
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            t.Cancel(true);
+        }
+    }
+}
+
---
+++ AsyncTaskDemo/AsyncTaskDemo/MyAsyncTask.cs
@@ -0,0 +1,39 @@
+﻿using Android.OS;
+using Java.Lang;
+using Android.Graphics;
+
+namespace AsyncTaskDemo
+{
+    public class MyAsyncTask : AsyncTask
+    {
+        private static readonly Color[] colors = { Color.Red, Color.Green, Color.Blue };
+        private int counter = 0;
+
+        private Android.Views.View _v;
+        public MyAsyncTask(Android.Views.View v)
+        {
+            this._v = v;
+        }
+
+        protected override Object DoInBackground(params Object[] @params)
+        {
+            while (!IsCancelled)
+            {
+                PublishProgress(new Object[] { counter++ });
+                if (counter > 2)
+                {
+                    counter = 0;
+                }
+                Thread.Sleep(2000);
+            }
+            return null;
+        }
+
+        protected override void OnProgressUpdate(params Object[] values)
+        {
+            Android.Util.Log.Debug("", values[0].ToString());
+            Color col = colors[(int)values[0]];
+            _v.SetBackgroundColor(col);
+        }
+    }
+}
