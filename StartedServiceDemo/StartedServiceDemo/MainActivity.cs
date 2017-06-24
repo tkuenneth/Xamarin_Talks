6b3d632a9dac921a015f737b18e7f6c4f7c8e417
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2017-06-24 17:24:18 +0200

    StartedServiceDemo added

---
+++ StartedServiceDemo/StartedServiceDemo/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace StartedServiceDemo
+{
+    [Activity(Label = "StartedServiceDemo", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        Intent service = null;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            Button b = FindViewById<Button>(Resource.Id.button);
+            b.Click += delegate
+            {
+                service = new Intent(this, Java.Lang.Class.FromType(typeof(MyService)));
+                StartService(service);
+            };
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            StopService(service);
+            service = null;
+        }
+    }
+}
+
---
+++ StartedServiceDemo/StartedServiceDemo/MyService.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+
+namespace StartedServiceDemo
+{
+    [Service(Label = "MyService")]
+    public class MyService : IntentService
+    {
+        static readonly string TAG = typeof(MyService).Name;
+
+        private bool shouldRun;
+        private long counter;
+        public MyService() : base("MyService")
+        {
+            shouldRun = true;
+            counter = 0;
+        }
+
+        protected override void OnHandleIntent(Intent intent)
+        {
+            while (shouldRun) {
+                Android.Util.Log.Debug(TAG, $"{counter++}");
+                Java.Lang.Thread.Sleep(2000);
+            }
+        }
+
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+            shouldRun = false;
+        }
+    }
+}
