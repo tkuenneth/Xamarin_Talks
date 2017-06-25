0ac1c3e92c0db8d84c9691444c1c6ecfb0e83929
Thomas Kuenneth <thomas.kuenneth@mathema.de>
2017-06-25 11:22:38 +0200

    JobSchedulerDemo added

---
+++ JobSchedulerDemo/JobSchedulerDemo/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.App.Job;
+using Android.Content;
+using Java.Lang;
+
+namespace JobSchedulerDemo
+{
+    [Activity(Label = "JobSchedulerDemo", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            Button b = FindViewById<Button>(Resource.Id.button);
+            b.Click += delegate
+            {
+
+                ComponentName cn = new ComponentName(this, Class.FromType(typeof(MyJobService)));
+                JobInfo.Builder builder = new JobInfo.Builder(123, cn);
+
+                long millis = 5 * 60 * 1000;
+                builder.SetPeriodic(millis);
+
+                JobScheduler js = (JobScheduler)GetSystemService(Context.JobSchedulerService);
+                js.Schedule(builder.Build());
+            };
+
+        }
+    }
+}
+
---
+++ JobSchedulerDemo/JobSchedulerDemo/MyJobService.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.App.Job;
+using Android.Util;
+
+namespace JobSchedulerDemo
+{
+    [Service(Exported = true, Name = "com.thomaskuenneth.MyJobService", Permission = "android.permission.BIND_JOB_SERVICE")]
+    class MyJobService : JobService
+    {
+        private static readonly string TAG = typeof(MyJobService).Name;
+
+        public override bool OnStartJob(JobParameters @params)
+        {
+            Log.Info(TAG, "started");
+            // work to be done
+            return true;
+        }
+
+        public override bool OnStopJob(JobParameters @params)
+        {
+            Log.Info(TAG, "started");
+            // re-schedule
+            return true;
+        }
+    }
+}
